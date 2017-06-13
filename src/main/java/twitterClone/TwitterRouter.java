26adb56b53c52b8b421c15307fd05621b8287dd9
john ulmer <john.ulmer@libertymutual.com>
2017-06-13 12:02:50 -0400

    java classes and one template

---
+++ src/main/java/twitterClone/TwitterRouter.java
@@ -0,0 +1,51 @@
+package twitterClone;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import java.util.ArrayList;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+import com.google.gson.Gson;
+
+public class TwitterRouter {
+
+    public static void main(String[] args) {
+        port(3000);
+        //AlbumList albumList = new AlbumList();
+
+//        get("/album/:id", (req, res) -> {
+//            System.out.println("request made");
+//            System.out.println(req.queryParams("b"));
+//            return "hi world2";
+//        });
+        get("/login", (request, response) -> {
+	        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/login.jtwig");
+	        JtwigModel model = JtwigModel.newModel();
+	        //JtwigModel model = JtwigModel.newModel().with("albums", albumList.albums);
+	        return template.render(model);
+        });
+        
+/*        
+        
+        get("/album/:id", (request, response) -> {
+            Album album = albumList.getAlbum(Integer.parseInt(request.params(":id")));
+            if (album == null){
+                return "Album was not found.";
+            }
+            return ("Hello: "+album.title+" "+album.artist+" "+album.genre);
+        });
+
+        get("/addAlbum/:title/:artist/:genre", (request, response) -> {
+            int newID = albumList.addAlbum(request.params(":title"), request.params(":artist"), request.params(":genre"));
+            return (newID);
+        });
+        
+
+        
+        get("/gsonAlbums", (req, res) -> {
+            Gson gson = new Gson();
+            System.out.println(gson.toJson(albumList.albums));
+            return gson.toJson(albumList.albums);
+        });*/
+}
+}
---
+++ src/main/java/twitterClone/User.java
@@ -0,0 +1,5 @@
+package twitterClone;
+
+public class User {
+
+}
